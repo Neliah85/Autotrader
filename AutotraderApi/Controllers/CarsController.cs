57e0058a8d6836024e707a8244962ac2556be4d9
Zsuzsanna <lengyel.zsuzsanna@gmail.com>
2025-01-30 20:17:23 +0100

    Add project files.

---
+++ AutotraderApi/Controllers/CarsController.cs
@@ -0,0 +1,33 @@
+﻿using AutotraderApi.Models;
+using AutotraderApi.Models.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutotraderApi.Controllers
+{
+    [Route("cars")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult AddNewCar(CreateCarDto createCarDto)
+        {
+            var car = new Car
+            {
+                Id = Guid.NewGuid(),
+                Brand = createCarDto.Brand,
+                Type = createCarDto.Type,
+                Color = createCarDto.Color,
+                Myear = createCarDto.Myear
+            };
+
+            using (var context = new AutotraderContext())
+            {
+                context.Cars.Add(car);
+                context.SaveChanges();
+
+                return StatusCode(201, new { result = car, message = "Sikeres felvétel." });
+            }
+
+        }
+    }
+}
---
+++ AutotraderApi/Models/Dtos/Dto.cs
@@ -0,0 +1,4 @@
+﻿namespace AutotraderApi.Models.Dtos
+{
+    public record CreateCarDto(string Brand, string Type, string Color, DateTime Myear);
+}
